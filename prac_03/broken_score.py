5210e3c93e259994907c415b521d3b3be63b7685
SeanSpring <sean_spring@live.com.au>
2019-08-18 20:43:19 +1000

    seperate function for score values

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -1,11 +1,17 @@
 def main():
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    #allows user to enter a score and find out its value
+    score = float(input("Enter score: "))
+    print(get_score_value(score))
+
+def get_score_value(score):
+    #users score is passed in and returns its value back to main
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
 main()
