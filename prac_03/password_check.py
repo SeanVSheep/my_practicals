775e741d12750b44598c051800dc20db3a70a00c
SeanSpring <sean_spring@live.com.au>
2019-08-18 19:46:53 +1000

    organised code to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -2,7 +2,23 @@
 Sean
 """
 MIN_LENGTH = 5
-password = input("Enter new password: ")
-while len(password) < MIN_LENGTH:
+def main():
+    #main body of code initialises functions to get password str and print password str length in artrics
+    password = get_password()
+    print_astrics(password)
+
+
+def print_astrics(password):
+    #receives password str and prints the same amount of characters with astrics
+    print(len(password) * "*")
+
+
+def get_password():
+    #user enters password and checks if the str is correct length
     password = input("Enter new password: ")
-print(len(password) * "*")
+    while len(password) < MIN_LENGTH:
+        password = input("Enter new password, minimum {} characters: ".format(MIN_LENGTH))
+    return password
+
+
+main()
