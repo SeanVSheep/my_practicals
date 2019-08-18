0bae895d747b57e8ab9912dee5b5092af964a631
SeanSpring <sean_spring@live.com.au>
2019-08-18 20:28:45 +1000

    celsius function added

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -1,19 +1,32 @@
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "C":
-        celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
-    elif choice == "F":
-        fahrenheit = float(input("fahrenheit: "))
-        celsius = (fahrenheit - 32) * 5 / 9
-        print("Result: {:.2f} F".format(celsius))
-    else:
-        print("Invalid option")
+
+def main():
+    #controls the temperature converation program
     print(MENU)
     choice = input(">>> ").upper()
-print("Thank you.")
+
+    while choice != "Q":
+
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            fahrenheit = method_name(celsius)
+            print("Result: {:.2f} F".format(fahrenheit))
+
+        elif choice == "F":
+            fahrenheit = float(input("fahrenheit: "))
+            celsius = (fahrenheit - 32) * 5 / 9
+            print("Result: {:.2f} F".format(celsius))
+
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+def method_name(celsius):
+    #users input for fahrenheit is passed in and returns celsius conversation
+    return celsius * 9.0 / 5 + 32
+
+main()
