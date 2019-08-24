5d0d1d95055910f34d4513a83b707874fb6d4c1a
SeanSpring <sean_spring@live.com.au>
2019-08-24 20:06:51 +1000

    error checks users input

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -6,7 +6,26 @@ How many quick picks? 5
 13 21 28 29 42 43
  3  4 10 11 32 44
 """
+
+import random
+NUMBERS_ON_A_LINE = 6
+
+MIN_RANGE = 1
+MAX_RANGE = 45
+
 def main():
+    exitLoop = False
+    while not exitLoop:
+        try:
+            picks = int(input("How many quick picks? "))
+            while picks <= 0:
+                picks = int(input("Picks must be greater then 0: "))
+            exitLoop = True
+        except ValueError:
+            print("ERROR! Please enter numerical value")
+
+
+
 
 
 
