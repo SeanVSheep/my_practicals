42af6ecbf68416215c8f64eb18cd798a2647ef9e
SeanSpring <sean_spring@live.com.au>
2019-08-28 18:04:38 +1000

    testing dict comprehensions

--- pass_and_handwritten/week_5/dict_test.py
+++ pass_and_handwritten/week_5/dict_test.py
@@ -35,9 +35,46 @@ def dict_activity():
     name = input("Name: ")
     age = int(input("Age: "))
     ages_dict[name] = age
-    for char in ages_dict:
-        print("{:10} - {:6}".format(char, ages_dict[char]))
+
+    for name in ages_dict:
+        print("{:10} - {:6}".format(name, ages_dict[name]))
+
+    for name, age in ages_dict.items():
+        print("{:10} - {:6}".format(name, age))
+
+#dict_activity()
+
+def test_func():
+    #hello = zip("abc", [1, 2, 3])
+    #hello looks like this [('a', 1), ('b', 2) ('c', 3)]
+    goodbye = dict(zip("abc", [1, 2, 3]))
+    print(goodbye)
+
+#test_func()
+
+
+def dict_comprehension():
+    dict_ages = {"Bill": 17, "Jane": 34, "Jack": 56, "Sven": 13}
+
+    new_dict = {name: age for name, age in dict_ages.items() if age < 18}
+
+    print(new_dict)
+
+    test_dict = {k:v for k,v in dict_ages.items()}
+
+    print(test_dict)
+
+    test_dict2 = {v:k for k,v in dict_ages.items()}
+
+    print(test_dict2)
+
+dict_comprehension()
 
 
 
-dict_activity()
+
+
+
+
+
+
