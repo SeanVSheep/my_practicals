011a6076bdf6cd8f563f6c75bcd59b81ce3d10a3
SeanSpring <sean_spring@live.com.au>
2019-08-29 18:39:17 +1000

    hex colour dictionary created

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,11 @@
+"""
+CP1404 Practical
+hex colours in a dictionary
+"""
+
+HEX_COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
+
+def main():
+
+
+main()
