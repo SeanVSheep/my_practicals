e380c7ee0002f8dc436c16b402d735c9ec035df5
SeanSpring <sean_spring@live.com.au>
2019-08-29 18:03:26 +1000

    have removed pass and handwritten folder to own respiratory

--- pass_and_handwritten/week_4/pass_week_3A.py
+++
@@ -1,6 +0,0 @@
-names = ['sean', 'steph']
-degree = ['IT', 'PHD']
-age = [26, 23]
-
-for i in range(len(names)):
-    print("name = {:15}\tdegree = {:15}\tage = {:>3}".format(names[i], degree[i], age[i]))
