10ca601c4294d6b32aa31847e79180bbdbfb0d0f
SeanSpring <sean_spring@live.com.au>
2019-08-29 18:49:40 +1000

    users can now get hex values of colours in dict

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,9 +3,19 @@ CP1404 Practical
 hex colours in a dictionary
 """
 
-HEX_COLOURS = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb", "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378", "aquamarine1": "#7fffd4", "aquamarine2": "#76eec6", "aquamarine4": "#458b74", "azure1": "#f0ffff"}
+HEX_COLOURS = {"alice blue": "#f0f8ff", "antique white": "#faebd7", "antique white1": "#ffefdb", "antique white2": "#eedfcc", "antique white3": "#cdc0b0", "antique white4": "#8b8378", "aqua marine1": "#7fffd4", "aqua marine2": "#76eec6", "aqua marine4": "#458b74", "azure1": "#f0ffff"}
 
 def main():
+    colour_name = input("What colour would you like the hex value of: ")
 
+    while colour_name != " ":
+        try:
+            print(HEX_COLOURS[colour_name])
+        except KeyError:
+            print("Hex value not defined")
+
+        colour_name = input("What colour would you like the hex value of: ")
+
+    print('Goodbye')
 
 main()
