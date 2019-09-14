4eaec9889f03dac69d8155cc8111d5e855d10b5b
SeanSpring <sean_spring@live.com.au>
2019-09-14 13:51:46 +1000

    initial commit of languages.py

---
+++ week_06/languages.py
@@ -0,0 +1,20 @@
+"""CP1404 Practical - Languages prac"""
+
+from week_06.programming_language import ProgrammingLanguage
+
+def main():
+
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+
+
+
+
+
+
+
+
+main()
