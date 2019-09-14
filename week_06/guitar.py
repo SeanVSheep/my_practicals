5e1e9bbeebe28282da8147251bc5963d56c2f8e6
SeanSpring <sean_spring@live.com.au>
2019-09-14 14:22:05 +1000

    test code added to guitar.py

--- week_06/guitar.py
+++ week_06/guitar.py
@@ -2,7 +2,7 @@
 
 class Guitar:
 
-    def __init__(self, name="", year=0, cost=0):
+    def __init__(self, name="", year=0, cost=0.0):
         self.name = name
         self.year = year
         self.cost = cost
@@ -11,6 +11,18 @@ class Guitar:
         return "{} ({}) : ${:.2f}".format(self.name, self.year, self.cost)
 
     def get_age(self):
-        return "The {} is 2019 - {} = {}".format(self.name, self.year, str(2019 - self.year))
+        return (2019 - self.year)
 
+    def is_vintage(self):
+        return self.get_age() >= 50
 
+def main():
+    """ test code for guitar class"""
+
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    print(gibson)
+
+    print(gibson.get_age())
+    print(gibson.is_vintage())
+
+main()
