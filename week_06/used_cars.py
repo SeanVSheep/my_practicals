1742aab0e697e483043d4876da81274a6a0fe75e
SeanSpring <sean_spring@live.com.au>
2019-09-14 13:32:41 +1000

    Updated car.py to include class holding __str__ and name held in __init_ added limo car feature to used_cars.py

--- week_06/used_cars.py
+++ week_06/used_cars.py
@@ -6,6 +6,8 @@ from week_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
+
+    '''
     my_car = Car(180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
@@ -14,6 +16,23 @@ def main():
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    '''
+
+    # MOD 1)
+    limo = Car("limo", 100)
+    print("Your {} has {}km worth of fuel".format(limo.name, limo.fuel))
+    # MOD 2)
+    limo.add_fuel(20)
+    print("You fill up with 20km of fuel, Your {} has {}km worth of fuel".format(limo.name, limo.fuel))
+    # MOD 3)
+    print("limo fuel =", limo.fuel)
+    # MOD 4)
+    limo.drive(115)
+    print("Your {} drives {}km".format(limo.name, limo.odometer))
+    # MOD 5)
+    print("limo odometer =", limo.odometer)
+    # MOD 6)
+    print(limo)
 
 
 main()
