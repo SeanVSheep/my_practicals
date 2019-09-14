e9a816f31a9d39462ea2010064685c789c35bc40
SeanSpring <sean_spring@live.com.au>
2019-09-14 13:39:47 +1000

    created ProgrammingLanguage class

---
+++ week_06/programming_language.py
@@ -0,0 +1,19 @@
+"""CP1404 Practical - Programming Language class"""
+
+
+class ProgrammingLanguage:
+    """Programming Language Class"""
+
+    def __init__(self, name, typing, reflection, year):
+        """"Programming Language"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+
+
+
+
+
+
