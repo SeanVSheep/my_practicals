eeabdcb15e23cf04147797f02ddf34ee4f46d13a
SeanSpring <sean_spring@live.com.au>
2019-09-14 15:09:01 +1000

    main now has 2 fixed guitars in a list, user can enter more if they choose.
    prints out list in formatted layout

--- week_06/guitars.py
+++ week_06/guitars.py
@@ -4,6 +4,10 @@ from week_06.guitar import Guitar
 
 def main():
     guitars = []
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
     name = input("Name: ")
     while name.strip() != "":
         year = int(input("Year: "))
@@ -13,7 +17,14 @@ def main():
         print(guitar)
         name = input("Name: ")
 
-    print(guitars)
+
+    for idx, guitar in enumerate(guitars, 1):
+        #print("Guitar {}: {}".format(idx, guitar))
+        print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(idx, guitar.name, guitar.year, guitar.cost, "(vintage)" if guitar.is_vintage() else ""))
+        #print("{guitar}".format(guitar=guitar))
+
+
+
 
 main()
 
