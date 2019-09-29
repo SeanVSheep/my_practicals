449b44a22f420d8093cd195adaeee2f6d9fc22b7
SeanSpring <sean_spring@live.com.au>
2019-09-29 19:22:26 +1000

    created menu choice function

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -3,6 +3,7 @@ from prac_08.silver_service_taxi import SilverServiceTaxi
 
 
 MENU = "q)uit, c)hoose taxi, d)rive"
+MENU_CHOICES = "qcd"
 
 
 def main():
@@ -10,8 +11,23 @@ def main():
     current_taxi = None
     bill = 0.00
     print("lets Drive!")
-    menu_choice = get_menu_choice()
+    menu_input = get_menu_choice()
+
+    while menu_input != 'q':
+
+        if menu_input == 'c':
+            print(taxis)
+
+        menu_input = get_menu_choice()
 
 
+
+
+
+def get_menu_choice():
+    user_choice = input("{}\n>>>".format(MENU)).lower()
+    while user_choice not in MENU_CHOICES:
+        user_choice = input("Invalid Option\n{}\n>>>".format(MENU)).lower()
+    return user_choice
 
 main()
