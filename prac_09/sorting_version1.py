f0589c2bd4e0d1de4846cd8f51a7e86bd7aa7910
SeanSpring <sean_spring@live.com.au>
2019-10-13 20:50:56 +1000

    finalised sorting_version1

--- prac_09/sorting_version1.py
+++ prac_09/sorting_version1.py
@@ -3,7 +3,18 @@ import os
 
 
 def main():
-    print("Starting directory is: {}".format(os.getcwd()))
+    os.chdir('FilesToSort')
+
+    file_names = [name for name in os.listdir('.')]
+    print(file_names)
+
+    for name in file_names:
+        try:
+            os.mkdir(name[name.find('.')+1:])
+        except FileExistsError:
+            pass
+
+        shutil.move(name, name[name.find('.')+1:])
 
 
 
